1bae9daf61ae578ac46c310b6713632144d14560
Luis <luis_baltodano@ucne.edu.do>
2022-06-24 21:48:24 -0400

    AgregandoUsuariosAlOnModelCreating

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -12,12 +12,73 @@ namespace DAL
 		public DbSet<TipoPagos> TipoPagos {get; set;}
 		public DbSet<Clientes> Clientes {get; set;}
 
+		public DbSet<Usuarios> Usuarios {get; set;}
+
+
+
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 			optionsBuilder.UseSqlite(@"Data Source= DATA/TelecableBeavers.db");
 		}
 		protected override void OnModelCreating(ModelBuilder builder)
 		{
+
+			builder.Entity<Usuarios>().HasData(
+				new Usuarios
+				{
+					UsuarioId = 1,
+					Nombres = "Luis Rafael Baltodano",
+					UserName = "RafaelB",
+					Password = "20200070"
+				}
+				
+			);
+
+			builder.Entity<Usuarios>().HasData(
+				new Usuarios
+				{
+					UsuarioId = 2,
+					Nombres = "Jeison Reyes",
+					UserName = "JeisonR",
+					Password = "20190564"
+				}
+				
+			);
+
+            builder.Entity<Usuarios>().HasData(
+                new Usuarios
+                {
+                    UsuarioId = 3,
+                    Nombres = "Samuel Duran",
+                    UserName = "SamuelD",
+                    Password = "20190793"
+                }
+
+            );
+
+            builder.Entity<Usuarios>().HasData(
+                new Usuarios
+                {
+                    UsuarioId = 4,
+                    Nombres = "Elianny Rosario",
+                    UserName = "EliannyR",
+                    Password = "20190255"
+                }
+
+            );
+
+            builder.Entity<Usuarios>().HasData(
+               new Usuarios
+               {
+                   UsuarioId = 5,
+                   Nombres = "Yunilda Justo",
+                   UserName = "YunildaJ",
+                   Password = "20190274"
+               }
+
+           );
+
+
 			builder.Entity<TipoPlanes>().HasData(new TipoPlanes
 			{
 				TipoPlanId = 1,
